d3912ec579c68a8e904fd893ab45999efdb12d07
amadden80 <amadden80@gmail.com>
2014-10-23 17:53:51 -0400

    shake it speare launch

--- controllers/shake_it_speare_controller.rb
+++ controllers/shake_it_speare_controller.rb
@@ -2,6 +2,9 @@
 
 class ShakeItSpeareController < Sinatra::Base
 
+  set :views, File.expand_path('../../views', __FILE__)
+  set :public_folder, File.expand_path('../../public', __FILE__)
+
   get '/' do
     erb :'documentation/index'
   end
